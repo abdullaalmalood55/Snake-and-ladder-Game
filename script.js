8d9172a5041b47e9bded36c2d321c41d24cd139b
Abdulla AlMalood <abdullaalmalood55@gmail.com>
2025-09-29 23:41:58 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const players = document.querySelector(".player")
+const squares = document.querySelectorAll(".square")
+const board = []
+let position = 0
+const boardlength = () => {
+  for (let i = 0; i < 100; i++) {
+    board.push(i + 1)
+  }
+}
+
+const updateboard = () => {
+  boardlength()
+  board.forEach((square, index) => {
+    squares[index].innerText = board[index]
+  })
+
+  console.log(board)
+  squares[0].appendChild(players)
+}
+
+updateboard()
