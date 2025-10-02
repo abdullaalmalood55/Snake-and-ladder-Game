cac1994056956bbb4916dda360e9f91eeb7b65c5
abdullaalmalood55 <abdullaalmalood55@gmail.com>
2025-10-02 23:38:40 +0300

    Moving the player in the board

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const players = document.querySelector(".player")
 const squares = document.querySelectorAll(".square")
+const Dice = document.querySelector(".dice")
+const diceValue = document.querySelector(".dice-value")
 const board = []
-let position = 0
+let playerposition = 0
 const boardlength = () => {
   for (let i = 0; i < 100; i++) {
     board.push(i + 1)
@@ -10,12 +12,30 @@ const boardlength = () => {
 
 const updateboard = () => {
   boardlength()
-  board.forEach((square, index) => {
-    squares[index].innerText = board[index]
-  })
-
-  console.log(board)
-  squares[0].appendChild(players)
+  board.forEach((square, index) => {})
 } /** */
+squares[playerposition].appendChild(players)
+const RollTheDice = () => {
+  const roll = Math.floor(Math.random() * 6) + 1
+  diceValue.innerText = roll
 
-updateboard()
+  playerposition += roll
+
+  if (playerposition >= 99) {
+    squares[99].appendChild(players)
+    return
+  }
+  squares[playerposition].appendChild(players)
+}
+Dice.addEventListener("click", () => RollTheDice())
+
+//End of add event listener
+
+const render = () => {
+  updateboard()
+}
+
+const gameInit = () => {
+  render()
+}
+gameInit()
